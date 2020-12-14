d995a38fc752928d1339a03f7f9869c1a0804937
kavyamittal1234 <68001757+kavyamittal1234@users.noreply.github.com>
2020-12-15 00:17:34 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,16 @@
+ 
+class Pig extends BaseClass{
+  constructor(x, y){
+    super(x,y,50,50);
+    //this.image = loadImage("sprites/enemy.png");
+  }
+display(){
+  fill("lightblue");
+   if (this.body.speed<5) {
+ super.display();  
+  }
+  else{
+    World.remove(world,this.body);
+  }
+}
+};
